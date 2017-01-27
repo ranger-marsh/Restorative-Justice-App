7a1ab1e6967dbb2d0bffe6a6df898efb4c6c3ec3
Scott <scott48074@users.noreply.github.com>
2017-01-27 10:59:44 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Restorative-Justice_App',
+    version='1.0',
+    packages=['app',],
+    license='MIT License',
+    long_description=open('README.txt').read(),
+)
