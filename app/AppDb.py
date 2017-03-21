2220c5f8139ed64427709fdfb7e4d86a1208c6eb
Scott <scott48074@users.noreply.github.com>
2017-03-21 06:27:02 -0500

    Added status to db table.

--- app/AppDb.py
+++ app/AppDb.py
@@ -22,16 +22,19 @@ class AppDb:
                         CREATE TABLE cases(id INTEGER PRIMARY KEY, case_number TEXT, case_date TEXT,
                         incident TEXT, ori TEXT, age TEXT, arrest_type TEXT, name TEXT,
                         address TEXT, apartment TEXT, city TEXT, state TEXT, dob TEXT, phone TEXT,
-                        race TEXT, sex TEXT, district TEXT)
+                        race TEXT, sex TEXT, district TEXT, status INTEGER)
                     ''')
         self.db.commit()
 
     def insert_row(self, row):
+        # data = [val for val in row]
+        # data.append(0)
         self.cursor.execute(
             '''INSERT INTO cases(case_number, case_date, incident, ori, age, arrest_type, name,
-                                address, apartment, city, state, dob, phone, race, sex, district)
-                                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', row)
+                                address, apartment, city, state, dob, phone, race, sex, district,status)
+                                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', row)
         self.db.commit()
+
 
     def close_db(self):
         self.db.close()
--- app/test/testAppDb.py
+++ app/test/testAppDb.py
@@ -19,7 +19,7 @@ class TestAppDb:
 
     def test_create_table(self):
         expected = ['id', 'case_number', 'case_date', 'incident', 'ori', 'age', 'arrest_type',
-                    'name', 'address', 'apartment', 'city', 'state', 'dob', 'phone', 'race', 'sex', 'district']
+                    'name', 'address', 'apartment', 'city', 'state', 'dob', 'phone', 'race', 'sex', 'district','status']
         self.db.create_table()
         self.db.close_db()
         db = sqlite3.connect('{}/test_data/temp_db'.format(os.getcwd()))
@@ -30,7 +30,7 @@ class TestAppDb:
 
     def test_write_row(self):
         test_row = ['1', '2', '3', '4', '5', '6', '7', '8',
-                    '9', '10', '11', '12', '13', '14', '15', '16']
+                    '9', '10', '11', '12', '13', '14', '15', '16', 0]
         self.db.create_table()
         self.db.insert_row(test_row)
         self.db.insert_row(test_row)
