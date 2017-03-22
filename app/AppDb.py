8b58b327a62168c831d4843280455d7061b8a314
Scott <scott48074@users.noreply.github.com>
2017-03-22 15:43:07 -0500

    added update_status and started membership check

--- app/AppDb.py
+++ app/AppDb.py
@@ -14,7 +14,6 @@ class AppDb:
                 create_table()
         else:
             self.db = sqlite3.connect(test_path)
-
         self.cursor = self.db.cursor()
 
     def open_csv(self, path):
@@ -29,15 +28,16 @@ class AppDb:
                         CREATE TABLE cases(id INTEGER PRIMARY KEY, case_number TEXT, case_date TEXT,
                         incident TEXT, ori TEXT, age TEXT, arrest_type TEXT, name TEXT,
                         address TEXT, apartment TEXT, city TEXT, state TEXT, dob TEXT, phone TEXT,
-                        race TEXT, sex TEXT, district TEXT, status INTEGER DEFAULT 0)
+                        race TEXT, sex TEXT, subject_type TEXT, district TEXT, status INTEGER DEFAULT 0)
                     ''')
         self.db.commit()
 
     def insert_rows(self, row):
         self.cursor.executemany(
             '''INSERT INTO cases(case_number, case_date, incident, ori, age, arrest_type, name,
-                                address, apartment, city, state, dob, phone, race, sex, district)
-                                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', row)
+                                address, apartment, city, state, dob, phone, race, sex, subject_type,
+                                district)
+                                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', row)
         self.db.commit()
 
     def query_db(self, query):
@@ -50,6 +50,14 @@ class AppDb:
             "SELECT * FROM cases WHERE id=?", [(row_id)]).fetchone()[-1]
         status += curr_status
         self.cursor.execute("UPDATE cases SET status=? WHERE id=?", (status, row_id))
+        self.db.commit()
+
+    def check_membership(self, row):
+        sql = "SELECT * FROM cases WHERE case_number=? AND name=?"
+        results = self.cursor.execute(sql, (row[0], row[6])).fetchone()
+        if results:
+            self.cursor.execute("DELETE FROM cases WHERE id=?", (results[0],))
+            self.db.commit()
 
     def close_db(self):
         self.db.close()
--- app/test/testAppDb.py
+++ app/test/testAppDb.py
@@ -29,7 +29,8 @@ class TestAppDb:
 
     def test_create_table(self):
         expected = ['id', 'case_number', 'case_date', 'incident', 'ori', 'age', 'arrest_type',
-                    'name', 'address', 'apartment', 'city', 'state', 'dob', 'phone', 'race', 'sex', 'district', 'status']
+                    'name', 'address', 'apartment', 'city', 'state', 'dob', 'phone', 'race', 'sex',
+                    'subject_type', 'district', 'status']
         self.db.create_table()
         self.db.close_db()
         db = sqlite3.connect('{}/test_data/temp_db'.format(os.getcwd()))
@@ -40,7 +41,7 @@ class TestAppDb:
 
     def test_write_row(self):
         test_row = ['1', '2', '3', '4', '5', '6', '7', '8',
-                    '9', '10', '11', '12', '13', '14', '15', '16']
+                    '9', '10', '11', '12', '13', '14', '15', '16', '17']
 
         test_many = list()
         test_many.append(test_row)
@@ -53,13 +54,13 @@ class TestAppDb:
         db = sqlite3.connect('{}/test_data/temp_db'.format(os.getcwd()))
         cursor = db.execute('select * from cases')
         assert cursor.fetchone()[1:] == ('1', '2', '3', '4', '5', '6', '7', '8',
-                                         '9', '10', '11', '12', '13', '14', '15', '16', 0)  # [0] is the row id
+                                         '9', '10', '11', '12', '13', '14', '15', '16', '17', 0)  # [0] is the row id
         assert cursor.fetchone()[0] == 2  # test row id increments.
         assert cursor.fetchone()[0] == 3  # test row id increments.
 
     def test_query_db(self):
         test_row = ['1', '2', '3', '4', '5', '6', '7', '8',
-                    '9', '10', '11', '12', '13', '14', '15', '16']
+                    '9', '10', '11', '12', '13', '14', '15', '16', '17']
 
         test_many = list()
         test_many.append(test_row)
@@ -69,14 +70,14 @@ class TestAppDb:
         self.db.insert_rows(test_many)
         assert len(self.db.query_db(0)) == 3
         assert self.db.query_db(0)[0] == (1, '1', '2', '3', '4', '5', '6', '7', '8',
-                                          '9', '10', '11', '12', '13', '14', '15', '16', 0)
+                                          '9', '10', '11', '12', '13', '14', '15', '16', '17', 0)
         assert self.db.query_db(0)[1] == (2, '1', '2', '3', '4', '5', '6', '7', '8',
-                                          '9', '10', '11', '12', '13', '14', '15', '16', 0)
+                                          '9', '10', '11', '12', '13', '14', '15', '16', '17', 0)
         self.db.close_db()
 
     def test_change_status(self):
         test_row = ['1', '2', '3', '4', '5', '6', '7', '8',
-                    '9', '10', '11', '12', '13', '14', '15', '16']
+                    '9', '10', '11', '12', '13', '14', '15', '16', '17']
 
         test_many = list()
         test_many.append(test_row)
@@ -93,3 +94,29 @@ class TestAppDb:
         assert len(self.db.query_db(1)) == 0
         assert len(self.db.query_db(11)) == 0
         self.db.close_db()
+
+    def test_check_membership(self):
+        test_rowa = ['2015-57325012', '10/11/2015', 'Drug Incident/Investigation', 'NQ4054983', '26', '', 'Oliver Coleman',
+                     '28687 Mallard Hill', 'c66', 'Napnapan', 'CA', '10/30/1998', '63-(829)189-2968', 'White', 'Male', '', 'Central']
+
+        test_rowb = ['2017-57325012', '10/11/2015', 'Drug Incident/Investigation', 'NQ4054983', '26', '', 'Oliver Coleman',
+                     '28687 Mallard Hill', 'c66', 'Napnapan', 'CA', '10/30/1998', '63-(829)189-2968', 'White', 'Male', '', 'Central']
+
+        test_rowc = ['2017-57325012', '10/11/2015', 'Drug Incident/Investigation', 'NQ4054983', '26', '', 'Scott Frasier',
+                     '28687 Mallard Hill', 'c66', 'Napnapan', 'CA', '10/30/1998', '63-(829)189-2968', 'White', 'Male', '', 'Central']
+
+        test_rowd = ['2019-57325012', '10/11/2015', 'Drug Incident/Investigation', 'NQ4054983', '26', '', 'Rupert Frasier',
+                     '28687 Mallard Hill', 'c66', 'Napnapan', 'CA', '10/30/1998', '63-(829)189-2968', 'White', 'Male', '', 'Central']
+
+        test_many = list()
+        test_many.append(test_rowa)
+        test_many.append(test_rowb)
+        test_many.append(test_rowc)
+
+        self.db.create_table()
+        self.db.insert_rows(test_many)
+        assert self.db.query_db(0)[0] == (1, '2015-57325012', '10/11/2015', 'Drug Incident/Investigation', 'NQ4054983', '26', '', 'Oliver Coleman',
+                                          '28687 Mallard Hill', 'c66', 'Napnapan', 'CA', '10/30/1998', '63-(829)189-2968', 'White', 'Male', '', 'Central', 0)
+        self.db.check_membership(test_rowa)
+        assert self.db.query_db(0)[0] != (1, '2015-57325012', '10/11/2015', 'Drug Incident/Investigation', 'NQ4054983', '26', '', 'Oliver Coleman',
+                                          '28687 Mallard Hill', 'c66', 'Napnapan', 'CA', '10/30/1998', '63-(829)189-2968', 'White', 'Male', '', 'Central', 0)
