115078f5aba6ed631d434738f4e57164af7f2c0d
Scott <scott48074@users.noreply.github.com>
2017-03-22 11:34:17 -0500

    csv reader built into AppDb.py

--- app/AppDb.py
+++ app/AppDb.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 import os
 
 
@@ -16,27 +17,39 @@ class AppDb:
 
         self.cursor = self.db.cursor()
 
+    def open_csv(self, path):
+        with open(path) as csvfile:
+            reader = list(csv.reader(csvfile, delimiter=','))
+            rows = [[val.strip().lower() for val in row] for row in reader[1:]]
+        return rows
+
     def create_table(self):
 
         self.cursor.execute('''
                         CREATE TABLE cases(id INTEGER PRIMARY KEY, case_number TEXT, case_date TEXT,
                         incident TEXT, ori TEXT, age TEXT, arrest_type TEXT, name TEXT,
                         address TEXT, apartment TEXT, city TEXT, state TEXT, dob TEXT, phone TEXT,
-                        race TEXT, sex TEXT, district TEXT, status INTEGER)
+                        race TEXT, sex TEXT, district TEXT, status INTEGER DEFAULT 0)
                     ''')
         self.db.commit()
 
     def insert_rows(self, row):
         self.cursor.executemany(
             '''INSERT INTO cases(case_number, case_date, incident, ori, age, arrest_type, name,
-                                address, apartment, city, state, dob, phone, race, sex, district,status)
-                                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', row)
+                                address, apartment, city, state, dob, phone, race, sex, district)
+                                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', row)
         self.db.commit()
 
     def query_db(self, query):
         sql = "SELECT * FROM cases WHERE status=?"
         self.cursor.execute(sql, [(query)])
         return self.cursor.fetchall()
+
+    def update_status(self, status, row_id):
+        curr_status = self.cursor.execute(
+            "SELECT * FROM cases WHERE id=?", [(row_id)]).fetchone()[-1]
+        status += curr_status
+        self.cursor.execute("UPDATE cases SET status=? WHERE id=?", (status, row_id))
 
     def close_db(self):
         self.db.close()
--- app/test/testAppDb.py
+++ app/test/testAppDb.py
@@ -13,6 +13,16 @@ class TestAppDb:
         self.db.close_db()
         os.remove('{}/test_data/temp_db'.format(os.getcwd()))
 
+    def test_open_csv(self):
+        rows = self.db.open_csv('{}/test_data/raw_test_data.csv'.format(os.getcwd()))
+        assert len(rows) == 20
+        for row in rows:
+            for val in row:
+                assert not val.startswith(' ')
+                assert not val.endswith(' ')
+                if val.isalpha():
+                    assert val.islower()
+
     def test_db_creation(self):
         db_file = Path('{}/test_data/temp_db'.format(os.getcwd()))
         assert db_file.is_file()
@@ -30,7 +40,7 @@ class TestAppDb:
 
     def test_write_row(self):
         test_row = ['1', '2', '3', '4', '5', '6', '7', '8',
-                    '9', '10', '11', '12', '13', '14', '15', '16', 0]
+                    '9', '10', '11', '12', '13', '14', '15', '16']
 
         test_many = list()
         test_many.append(test_row)
@@ -42,26 +52,44 @@ class TestAppDb:
         self.db.close_db()
         db = sqlite3.connect('{}/test_data/temp_db'.format(os.getcwd()))
         cursor = db.execute('select * from cases')
-        assert cursor.fetchone()[1:] == tuple(test_row)  # [0] is the row id
+        assert cursor.fetchone()[1:] == ('1', '2', '3', '4', '5', '6', '7', '8',
+                                         '9', '10', '11', '12', '13', '14', '15', '16', 0)  # [0] is the row id
         assert cursor.fetchone()[0] == 2  # test row id increments.
         assert cursor.fetchone()[0] == 3  # test row id increments.
 
     def test_query_db(self):
         test_row = ['1', '2', '3', '4', '5', '6', '7', '8',
-                    '9', '10', '11', '12', '13', '14', '15', '16', 0]
-        test_rowa = ['1', '2', '3', '4', '5', '6', '7', '8',
-                     '9', '10', '11', '12', '13', '14', '15', '16', 1]
+                    '9', '10', '11', '12', '13', '14', '15', '16']
 
         test_many = list()
         test_many.append(test_row)
         test_many.append(test_row)
-        test_many.append(test_rowa)
+        test_many.append(test_row)
         self.db.create_table()
         self.db.insert_rows(test_many)
-        assert len(self.db.query_db(0)) == 2
+        assert len(self.db.query_db(0)) == 3
         assert self.db.query_db(0)[0] == (1, '1', '2', '3', '4', '5', '6', '7', '8',
                                           '9', '10', '11', '12', '13', '14', '15', '16', 0)
         assert self.db.query_db(0)[1] == (2, '1', '2', '3', '4', '5', '6', '7', '8',
                                           '9', '10', '11', '12', '13', '14', '15', '16', 0)
-        assert test_rowa not in self.db.query_db(0)
+        self.db.close_db()
+
+    def test_change_status(self):
+        test_row = ['1', '2', '3', '4', '5', '6', '7', '8',
+                    '9', '10', '11', '12', '13', '14', '15', '16']
+
+        test_many = list()
+        test_many.append(test_row)
+        test_many.append(test_row)
+        test_many.append(test_row)
+        self.db.create_table()
+        self.db.insert_rows(test_many)
+        self.db.update_status(1, 3)
+        assert len(self.db.query_db(1)) == 1
+        self.db.update_status(10, 3)
+        assert len(self.db.query_db(11)) == 1
+        self.db.update_status(100, 3)
+        assert len(self.db.query_db(111)) == 1
+        assert len(self.db.query_db(1)) == 0
+        assert len(self.db.query_db(11)) == 0
         self.db.close_db()
