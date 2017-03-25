576b59ef4dc5137ad746d8a45cec1bc50941a36e
Scott <scott48074@users.noreply.github.com>
2017-03-25 06:23:04 -0500

    Added test for header validation.

--- app/test/test_csv_handler.py
+++ app/test/test_csv_handler.py
@@ -1,13 +1,17 @@
 import os
-
 import csv_handler
 
-def test_open_csv():
-        rows = csv_handler.open_csv('{}/test_data/raw_test_data.csv'.format(os.getcwd()))
-        assert len(rows) == 20
-        for row in rows:
-            for val in row:
-                assert not val.startswith(' ')
-                assert not val.endswith(' ')
-                if val.isalpha():
-                    assert val.islower()
+
+def test_open_valid_csv():
+    rows = csv_handler.open_csv('{}/test_data/raw_test_data.csv'.format(os.getcwd()))
+    assert len(rows) == 20
+    for row in rows:
+        for val in row:
+            assert not val.startswith(' ')
+            assert not val.endswith(' ')
+            if val.isalpha():
+                assert val.islower()
+
+
+def test_open_invalid_headers():
+    assert not csv_handler.open_csv('{}/test_data/bad_headers.csv'.format(os.getcwd()))
